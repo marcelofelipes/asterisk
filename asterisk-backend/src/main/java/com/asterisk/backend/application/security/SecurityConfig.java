dd5690934ff4856b654e8e561df8b5c0814569f6
Julian <julxott@outlook.de>
2022-06-07 17:21:34 +0200

    Adding actuator version endpoint

--- asterisk-backend/src/main/java/com/asterisk/backend/application/security/SecurityConfig.java
+++ asterisk-backend/src/main/java/com/asterisk/backend/application/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +17,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -36,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String AUTH = "/auth/**";
     private static final String LOGOUT = "/auth/logout";
     private static final String HEALTHCHECK = "/healthcheck";
+    private static final String VERSION = "/version";
     private static final String FAVICON = "/favicon.ico";
 
     private final AccessTokenFilter accessTokenFilter;
@@ -61,7 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers(LOGOUT).authenticated()
-                .antMatchers(AUTH, HEALTHCHECK, FAVICON).permitAll()
+                .antMatchers(AUTH, HEALTHCHECK, VERSION, FAVICON).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
